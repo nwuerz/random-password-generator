e685ccd58b71015efaaa404edd60f6c3a0a5f812
nick wuerz <nawuerz@gmail.com>
2020-01-08 13:35:25 -0600

    added id's to check boxes and onclick id's to buttons in html

--- generate.js
+++ generate.js
@@ -1,7 +1,17 @@
-<script>
 
-    var numberOptions = ["1","2","3","4","5","6","7","8","9","0"];
-    var letterOptions = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-    var characterOptions = ["!","'",'"',"#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
+var numberOptions = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var letterOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var characterOptions = ["!", "'", '"', "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 
-</script>
+// collect user input from "password length" section //
+function generatePassword() {
+    alert("you did it!!");
+}
+
+
+//if then statements depending on which optional characters are selected //
+
+
+//display password//
+
+//copy to clipboard if selected//
