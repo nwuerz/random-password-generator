4d9e1547e80db6626bf3e16b0cf57da008102beb
nick wuerz <nawuerz@gmail.com>
2020-01-09 16:24:25 -0600

    added javascript

--- generate.js
+++ generate.js
@@ -1,17 +1,47 @@
 
-var numberOptions = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var letterOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var characterOptions = ["!", "'", '"', "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+function generateButton() {
 
-// collect user input from "password length" section //
-function generatePassword() {
-    alert("you did it!!");
+    var passwordLength = prompt("Please select a password length between 8 and 128!");
+    var includeNumber = confirm("would you like to include numbers?");
+    var includeUpper = confirm("would you like to inlcude uppercase letters?");
+    var includeLower = confirm("would you like to include lowercase letters?");
+    var includeSpecial = confirm("would you like to include special characters?");
 }
 
+// select a random letter in string x amount of times, depending on userinput
 
-//if then statements depending on which optional characters are selected //
+// 4 strings with various characters depending on what user is looking for//
 
+var = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-//display password//
-
-//copy to clipboard if selected//
+// password combination options...
+//lowercase
+var = "abcdefghijklmnopqrstuvwxyz";
+//uppercase
+var = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//numbers
+var = "1234567890";
+//symbols (missing "")
+var = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+//lowercase & uppercase
+var = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//lowercase & uppercase & numbers
+var = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+//lowercase & uppercase & numbers & symbols
+var = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+//uppercase & numbers
+var = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+//uppercase & numbers & symbols
+var = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+//numbers & lowercase
+var = "abcdefghijklmnopqrstuvwxyz1234567890";
+//numbers & lowercase & symbols
+var = "abcdefghijklmnopqrstuvwxyz1234567890!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+//symbols & numbers
+var = "1234567890!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+//symbols & lowercase
+var = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~abcdefghijklmnopqrstuvwxyz";
+//symbols & uppercase
+var = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+// function that performs generates one of the above passwords based on user input
