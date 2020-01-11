aac813e9aa7e7e064b9a891b14c34241595ba1c2
nick wuerz <nawuerz@gmail.com>
2020-01-10 21:18:00 -0600

    included possible password combinations and function to select random characters in java

--- generate.js
+++ generate.js
@@ -1,47 +1,139 @@
+// password combination possibilities...
 
-function generateButton() {
+//lowercase
+var lower = "abcdefghijklmnopqrstuvwxyz";
+//uppercase
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//numbers
+var num = "1234567890";
+//symbols (missing "")
+var sym = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+//lowercase & uppercase 
+var lowerUpper = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//lowercase & uppercase & numbers 
+var lowerUpperNum = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+//lowercase & uppercase & numbers & symbols
+var all = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+//uppercase & numbers 
+var upperNum = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+//uppercase & numbers & symbols 
+var upperNumSym = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+//numbers & lowercase 
+var lowerNum = "abcdefghijklmnopqrstuvwxyz1234567890";
+//numbers & lowercase & symbols 
+var lowerNumSym = "abcdefghijklmnopqrstuvwxyz1234567890!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+//symbols & numbers 
+var numSym = "1234567890!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+//symbols & lowercase
+var symLower = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~abcdefghijklmnopqrstuvwxyz";
+//symbols & uppercase
+var upperSym = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
-    var passwordLength = prompt("Please select a password length between 8 and 128!");
-    var includeNumber = confirm("would you like to include numbers?");
-    var includeUpper = confirm("would you like to inlcude uppercase letters?");
-    var includeLower = confirm("would you like to include lowercase letters?");
-    var includeSpecial = confirm("would you like to include special characters?");
+
+// retreive info from user needed to generate password
+
+
+
+ var passwordLength = prompt("Please select a password length between 8 and 128!");
+
+ if (passwordLength >= 8 && passwordLength <=128) {
+
+     var includeLower = confirm("would you like to include lowercase letters?");
+     var includeUpper = confirm("would you like to inlcude uppercase letters?");
+     var includeNumber = confirm("would you like to include numbers?");
+     var includeSpecial = confirm("would you like to include special characters?");
+
 }
 
-// select a random letter in string x amount of times, depending on userinput
+else {
+    alert("please select a number between 8 and 128");
+    var passwordLength = prompt("Please select a password length between 8 and 128!");
+}
 
-// 4 strings with various characters depending on what user is looking for//
+//make a series of if/then statements for the various scenarios
 
-var = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+if (includeLower === true) {
+    chooseRandom(lower);
+    alert("your passord is " + randomSelection);
+}
 
-// password combination options...
-//lowercase
-var = "abcdefghijklmnopqrstuvwxyz";
-//uppercase
-var = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//numbers
-var = "1234567890";
-//symbols (missing "")
-var = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-//lowercase & uppercase
-var = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//lowercase & uppercase & numbers
-var = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-//lowercase & uppercase & numbers & symbols
-var = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-//uppercase & numbers
-var = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-//uppercase & numbers & symbols
-var = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-//numbers & lowercase
-var = "abcdefghijklmnopqrstuvwxyz1234567890";
-//numbers & lowercase & symbols
-var = "abcdefghijklmnopqrstuvwxyz1234567890!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-//symbols & numbers
-var = "1234567890!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-//symbols & lowercase
-var = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~abcdefghijklmnopqrstuvwxyz";
-//symbols & uppercase
-var = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-// function that performs generates one of the above passwords based on user input
+else if (includeUpper === true) {
+    alert(" ");
+}
+
+else if (includeNumber === true) {
+    alert(" ");
+}
+
+else if (includeSpecial === true) {
+    alert(" ");
+}
+
+else if (includeLower === true && includeUpper === true) {
+    alert("");
+}
+
+else if (includeLower === true && includeUpper === true && includeNumber === true) {
+    alert("");
+}
+
+else if (includeLower === true && includeUpper === true && includeNumber === true && includeSpecial === true ) {
+    alert("");
+}
+
+else if (includeUpper === true && includeNumber === true) {
+    alert("");
+}
+
+else if (includeUpper === true && includeNumber === true && includeSpecial === true) {
+    alert("");
+}
+
+else if (includeNumber === true && includeLower === true) {
+    alert("");
+}
+
+else if (includeNumber === true && includeLower === true && includeSpecial === true) {
+    alert("");
+}
+
+else if (includeSpecial === true && includeNumber === true) {
+    alert("");
+}
+
+else if (includeSpecial === true && includeLower === true) {
+    alert("");
+}
+
+else if (includeSpecial === true && includeUpper === true) {
+    alert("");
+}
+
+
+//choose a random letter in that the string 
+
+function chooseRandom(string) {
+    
+    for (var i = 0; i < passwordLength.length; i++) {
+
+      randomSelection = string.charAt(Math.floor(Math.random() * string.length));
+
+    }
+}
+
+//keep choosing a random letter for the amount of times that the user requested and concatenate them into a string
+
+
+
+
+
+
+// function generatePassword() {
+
+//     for (var i = 0; i < passwordLength.length; i++) {
+
+//         password = randomSelection * passwordLength;
+//     }
+    
+// }
+
